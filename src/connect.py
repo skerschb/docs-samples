6349e4e26508a8dd88e8d09a5a7428119e574f78
skerschb <sue.kerschbaumer@10gen.com>
2018-04-06 15:23:46 -0400

    connect

--- src/connect.py
+++ src/connect.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 class Connect():
     def get_connection(self):
         print "getting connection"
-        connection_string = "<connection-string>"
+        connection_string = "<URISTRING>"
         client = MongoClient(connection_string)
         return client
 #End Connect
