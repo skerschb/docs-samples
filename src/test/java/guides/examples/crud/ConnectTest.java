c8017552f3a9d6e2029806f5478953d63d144452
skerschb <sue.kerschbaumer@10gen.com>
2018-04-07 18:41:52 -0400

    added inventory bind

--- src/test/java/guides/examples/crud/ConnectTest.java
+++ src/test/java/guides/examples/crud/ConnectTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCollection;
 
 public class ConnectTest {
     
@@ -15,5 +16,12 @@ public class ConnectTest {
         Connect.closeConnection(client);
         //End Call Example
     }
-
+   
+    @Test
+    void testCollectionBinding() {
+        MongoClient mongoClient = Connection.getConnection();
+        //Start Collection Bind
+        MongoCollection collection = mongoClient.getCollection("inventory");
+        //End Collection Bind
+    }  
 }
