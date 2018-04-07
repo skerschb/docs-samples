64bca1d453ebc867a0d552ba4ea60dab6dbc5c2c
skerschb <sue.kerschbaumer@10gen.com>
2018-04-07 19:16:49 -0400

    connect

--- src/connecttest.py
+++ src/connecttest.py
@@ -4,6 +4,6 @@ from pymongo import MongoClient
 
 client = Connect()
 connection = client.get_connection()
+#End Caller Connect
 assert connection is  not None
 print connection
-#End Caller Connect
